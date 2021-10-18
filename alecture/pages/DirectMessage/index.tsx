81978daa7a682e834599aaf56b2015a53e5868eb
dongmin7208 <sayg3o3d@gmail.com>
2021-10-18 15:45:06 +0900

    update:DirectMessage- Route

---
+++ alecture/pages/DirectMessage/index.tsx
@@ -0,0 +1,12 @@
+import Workspace from '@layouts/Workspace';
+import React from 'react';
+
+const DirectMessage = () => {
+    return (
+        <Workspace>
+            <div>login おめでとうございます。</div>
+        </Workspace>
+    )
+};
+
+export default DirectMessage;
